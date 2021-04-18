c142424168f2fb71a17ea21dad5b7fe80524963e
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-04-18 15:14:34 +0530

    new components get created

---
+++ my-todos/src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Footer(props) {
+    return (
+        <div>
+            footer works
+        </div>
+    )
+}
+
+Footer.propTypes = {
+
+}
+
+export default Footer
+
---
+++ my-todos/src/MyComponents/Todos.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Todos(props) {
+    return (
+        <div>
+            Todos work
+        </div>
+    )
+}
+
+Todos.propTypes = {
+
+}
+
+export default Todos
